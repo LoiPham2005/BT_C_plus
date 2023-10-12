8cc623d3493a13bae7568f01cf10c1b35b7dee2b
LoiPham2005 <phamducloi919@gmail.com>
2023-10-12 23:36:28 +0700

    php

---
+++ tim so chan le.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int so;
+	cout<<"nhap vao so :";
+	cin>>so;
+	
+	if (so%2==0){
+		cout<<so<<" la so chan";
+	}
+	else {
+		cout<<so<<" la so le";
+	}
+	
+}
